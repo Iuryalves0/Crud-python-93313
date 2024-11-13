00b3718eab2d6ba407f5db816759aa1ea77ab470
Iuryalves0 <iuryalves2317@gmail.com>
2024-11-13 19:44:44 +0000

    salvar

--- app/config/database.py
+++ app/config/database.py
@@ -1,1 +1,30 @@
 #banco de dados
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+from contextlib import contextmanager
+
+#definindo parametros para acesso ao banco de dados.
+db_user = "aluno"
+db_password = ""aluno_senha"
+db_name = "meu_banco_senai"
+db_host = "localhost"
+db_port = "3306"
+
+#Endereço (URL) para conexão com banco de dados.
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+#criando banco de dados.
+db = create_engine(DATABASE_URL)
+
+#criando conexão com banco de dados.
+Session = sessionmaker(bind = db)
+session = Session()
+
+#Gerenciando conexão com banco de dados
+@contextmanager
+def get_db():
+    db = Session()
+    try:
+        yield db #realiza operação no BD.
+        db.commit()#salvando no BD.
+    except Exception as erro
---
+++ app/main.py
@@ -0,0 +1,4 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repositary import UsuarioRepository
+from config.database import Session
+import os
